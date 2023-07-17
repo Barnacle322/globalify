d045ac3a5deb61d69140f0236208657f6df53cf2
Arstanbek Usenov <arstan.usenov@gmail.com>
2023-07-17 16:05:16 +0600

    ✨ Added a cool animation

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,26 @@ module.exports = {
       gridTemplateColumns: {
         'auto-repeat': 'repeat(auto-fit, 12rem)',
         'auto-repeat-mobile': 'repeat(auto-fit, 9rem)',
+      },
+      keyframes: {
+        wiggle: {
+          '0%, 100%': { transform: 'rotate(-1deg)' },
+          '50%': { transform: 'rotate(1deg)' },
+        },
+        flyoff: {
+          '0%': {transform: 'translateX(0) translateY(0)'},
+          '25%': {transform: 'translateX(0) translateY(0)'},
+          '100%': {transform: 'translateX(1500px) translateY(-1500px)'}
+        },
+        fadeoff: {
+          '0%, 25%': {opacity: '0'},
+          '100%': {opacity: '1'},
+        }
+      }, 
+      animation: {
+        wiggle: 'wiggle 0.2s ease-in-out infinite',
+        flyoff: 'flyoff 3s ease-in forwards',
+        fadeoff: 'fadeoff 1s ease-in-out forwards'
       }
     },
   },

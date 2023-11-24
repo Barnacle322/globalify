bbf0af32cba5b971832eaa7d6832c5b9b0ffd7dd
Barnacle322 <arstan.usenov@gmail.com>
2023-11-24 10:53:28 +0600

    🚑  Payment fixed

--- server/src/project/routes/payment.py
+++ server/src/project/routes/payment.py
@@ -472,7 +472,7 @@ def payment_failed(data_object):
 
 
 def charge_succeeded(data_object):
-    if int(data_object.get("amount")) != 5000:
+    if int(data_object.get("amount")) != 10000:
         return jsonify(success=True)
 
     stripe_customer_id = data_object.get("customer")

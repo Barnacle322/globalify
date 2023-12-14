11a6bd9b54e1f2c13befb11c5544e7f9721894ab
daniiar-mamaev <daniiar.mamaev@gmail.com>
2023-12-14 16:24:51 +0600

    ✅ added tests for automatic DB population

---
+++ src/tests/models/test_helpers.py
@@ -0,0 +1,36 @@
+import pycountry
+
+from ...project.models import Country, Industry, Round
+from ...project.utils.info_lists import aggregate as industry_aggregate
+
+
+def test_country_database(app):
+    with app.app_context():
+        country_list = []
+        for country in pycountry.countries:
+            country_list.append((country.name, country.alpha_2))
+
+        assert Country.query.count() == len(pycountry.countries)
+        for i in range(1, Country.query.count() + 1):
+            assert Country.get_by_id(id=i).name == country_list[i - 1][0]
+            assert Country.get_by_id(id=i).code == country_list[i - 1][1]
+
+
+def test_industry_database(app):
+    with app.app_context():
+        industry_list = []
+        for category, industries in industry_aggregate.items():
+            list(map(lambda x: industry_list.append((x, category)), industries))
+
+        assert Industry.query.count() == len(industry_list)
+        for i in range(1, Industry.query.count() + 1):
+            assert Industry.get_by_id(id=i).name == industry_list[i - 1][0]
+            assert Industry.get_by_id(id=i).category == industry_list[i - 1][1]
+
+
+def test_round_database(app):
+    with app.app_context():
+        round_list = ["Pre-Seed", "Seed", "Series A", "Series B", "Series C"]
+        assert Round.query.count() == len(round_list)
+        for item in Round.query.all():
+            assert item.name in round_list

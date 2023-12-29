2d818cece5aca1f2b7df14db2aa96a262fcd9f32
Agahan14 <Imamidinov.agahan06@gmail.com>
2023-12-29 13:00:22 +0600

    deleted unnecessary file and updated some js files as in investor filtering

--- src/project/static/scripts/dashboard.js
+++ src/project/static/scripts/dashboard.js
@@ -1,9 +1,9 @@
 const menus = [
     { menu: "user-menu", button: "user-menu-button" },
     { menu: "industry-options", button: "industry-options-menu" },
-    { menu: "sorting-options", button: "sorting-options-menu" },
-    { menu: "filter-options", button: "filter-options-menu" },
     { menu: "round-options", button: "round-options-menu" },
+    { menu: "sorting-options", button: "sorting-options-menu" },
+    { menu: "filter-options", button: "filter-options-menu" },  
 ];
 
 const showClasses = ["transform", "opacity-100", "scale-100"];
@@ -13,7 +13,7 @@ menus.forEach(({ menu, button }) => {
     const menuElement = document.getElementById(menu);
     const buttonElement = document.getElementById(button);
 
-    document.addEventListener("onclick", (event) => {
+    document.addEventListener("click", (event) => {
         if (!menuElement.contains(event.target) && !buttonElement.contains(event.target)) {
             menuElement.classList.remove(...showClasses);
             menuElement.classList.add(...hideClasses);

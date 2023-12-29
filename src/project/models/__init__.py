aa3095694d16249bba8895a376d66a1b5589a622
Agahan14 <Imamidinov.agahan06@gmail.com>
2023-12-29 12:59:29 +0600

    ♻️ changed the structures of the models as in investor filtering

---
+++ src/project/models/__init__.py
@@ -0,0 +1,17 @@
+from .helpers import Country, Industry, Round
+from .investor import InvestmentFirm, Investor
+from .user import Company, User, UserInfo, UserPayment, Waitlist, WaitlistCharge
+
+__all__ = [
+    "Country",
+    "Industry",
+    "Round",
+    "InvestmentFirm",
+    "Investor",
+    "Company",
+    "User",
+    "UserInfo",
+    "UserPayment",
+    "Waitlist",
+    "WaitlistCharge",
+]

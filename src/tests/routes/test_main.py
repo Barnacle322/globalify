121e52f61d90efbf8c0553040feb7c681e63fab1
daniiar-mamaev <daniiar.mamaev@gmail.com>
2024-01-11 01:29:09 +0600

    ✅ added tests for download endpoint

--- src/tests/routes/test_main.py
+++ src/tests/routes/test_main.py
@@ -1,3 +1,8 @@
+from src.project.models.user import WaitlistCharge
+
+from ...project import db
+
+
 def test_index(client):
     response = client.get("/")
     assert response.status_code == 200
@@ -90,3 +95,105 @@ def test_download_refused(client):
     assert response.status_code == 200
     assert b"Huh! It looks like you may have already downloaded our list of investors." in response.data
     assert b"Please check your downloads folder." in response.data
+
+
+def test_download_already_downloaded(client, app):
+    with app.app_context():
+        waitlist_charge = WaitlistCharge(
+            stripe_customer_id="stripe_id",
+            charge_id="charge_id",
+            customer_email="cus@example.com",
+            customer_name="John Doe",
+            random_key="12345",
+            downloaded=True,
+        )
+        db.session.add(waitlist_charge)
+        db.session.commit()
+        response = client.get("/download/12345")
+        assert response.status_code == 200
+        assert b"Huh! It looks like you may have already downloaded our list of investors." in response.data
+        assert b"Please check your downloads folder." in response.data
+
+
+def test_download_success(client, app):
+    with app.app_context():
+        waitlist_charge = WaitlistCharge(
+            stripe_customer_id="stripe_id",
+            charge_id="charge_id",
+            customer_email="cus@example.com",
+            customer_name="John Doe",
+            random_key="12345",
+            downloaded=False,
+        )
+        db.session.add(waitlist_charge)
+        db.session.commit()
+        response = client.get("/download/12345")
+        assert response.status_code == 200
+        assert (
+            b"Download a curated list featuring lead investors perfectly matched for your startup in different industries."
+            in response.data
+        )
+        assert b"Please note, that you can only download this file" in response.data
+
+
+def test_download_post_wrong_key(client, app):
+    with app.app_context():
+        waitlist_charge = WaitlistCharge(
+            stripe_customer_id="stripe_id",
+            charge_id="charge_id",
+            customer_email="cus@example.com",
+            customer_name="John Doe",
+            random_key="12345",
+            downloaded=False,
+        )
+        db.session.add(waitlist_charge)
+        db.session.commit()
+    response = client.post("/download", data={"key": "54321"}, follow_redirects=True)
+    assert response.status_code == 200
+    assert b"Globalify is" in response.data
+    assert b"Tailored experience for specific regions" in response.data
+    assert b"Frequently asked questions" in response.data
+    assert b"From the blog" in response.data
+    assert b"Use Globalify - Fund Your Startup" in response.data
+
+
+def test_download_post_already_downloaded(client, app):
+    with app.app_context():
+        waitlist_charge = WaitlistCharge(
+            stripe_customer_id="stripe_id",
+            charge_id="charge_id",
+            customer_email="cus@example.com",
+            customer_name="John Doe",
+            random_key="12345",
+            downloaded=True,
+        )
+        db.session.add(waitlist_charge)
+        db.session.commit()
+    response = client.post("/download", data={"key": "12345"}, follow_redirects=True)
+    assert response.status_code == 200
+    assert b"Globalify is" in response.data
+    assert b"Tailored experience for specific regions" in response.data
+    assert b"Frequently asked questions" in response.data
+    assert b"From the blog" in response.data
+    assert b"Use Globalify - Fund Your Startup" in response.data
+
+
+def test_download_post_success(client, app):
+    with app.app_context():
+        waitlist_charge = WaitlistCharge(
+            stripe_customer_id="stripe_id",
+            charge_id="charge_id",
+            customer_email="cus@example.com",
+            customer_name="John Doe",
+            random_key="12345",
+            downloaded=False,
+        )
+        db.session.add(waitlist_charge)
+        db.session.commit()
+    response = client.post("/download", data={"random_key": "12345"}, follow_redirects=True)
+    content_disposition = response.headers.get("Content-Disposition", "")
+    assert "attachment" in content_disposition
+    with app.app_context():
+        updated_waitlist_charge = WaitlistCharge.get_by_random_key("12345")
+        assert updated_waitlist_charge
+        assert updated_waitlist_charge.downloaded

f13c2b496f22568d2ef8affb5132145c507f2705
Agahan14 <Imamidinov.agahan06@gmail.com>
2024-01-30 08:23:23 +0600

    added profile page for user

--- src/project/routes/profile.py
+++ src/project/routes/profile.py
@@ -1,24 +1,18 @@
-import re
-
 from flask import Blueprint, abort, redirect, render_template, request, url_for
 from flask_login import current_user, fresh_login_required, login_required, logout_user
 
-from src.project.models.helpers import Country, Industry, Round
 from src.project.models.user import Company
 
-from ..extensions import db
-from ..models import User, UserInfo, UserOauth, UserPayment, UserRegular
-from ..utils.errors.auth_error_messages import AUTH_EMAIL_USED, AUTH_FIELDS_INCOMPLETE, AUTH_INVALID_EMAIL
+from ..models import User, UserInfo
 from ..utils.google_storage import load_pfp, prepare_picture, upload_blob, upload_pfp
 from ..utils.info_lists import languages as language_list
-from ..utils.status_enum import Status, StatusType, Tier
 from .main import check_user_info_complete, check_verification
 
 profile = Blueprint("profile", __name__)
 
 
 @profile.route("/me", methods=["GET"])
-def user_profile():
+def profile_me():
 
     if current_user.is_anonymous:
         return redirect(url_for("auth.login"))
@@ -27,6 +21,23 @@ def user_profile():
 
     user = User.get_by_id(authenticated_user.id)
     user_info = UserInfo.get_by_user_id(authenticated_user.id)
+
+    if user:
+        return render_template("profile/user_profile.html", user=user, user_info=user_info)
+    else:
+        abort(404)
+
+
+@profile.route("/<int:user_id>/", methods=["GET"])
+def user_profile(user_id):
+
+    user = User.get_by_id(user_id)
+    if not user:
+        abort(404)
+
+    user_info = UserInfo.get_by_user_id(user_id)
+    if not user_info:
+        abort(404)
 
     if user:
         return render_template("profile/user_profile.html", user=user, user_info=user_info)

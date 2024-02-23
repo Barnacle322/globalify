88531c331efaba3a9535c9deea06b56735af11d4
Barnacle322 <arstan.usenov@gmail.com>
2024-02-23 15:20:25 +0600

    🩹 Fix checkbox value in search.html and uncomment code in google_pubsub.py

--- src/project/utils/google_helpers/google_pubsub.py
+++ src/project/utils/google_helpers/google_pubsub.py
@@ -41,9 +41,8 @@ def get_callback(
 
 
 def send_event(msg: str, **attributes) -> None:
-    ...
-    # publish_future = publisher.publish(topic_path, msg.encode("utf-8"), **attributes)
-    # publish_future.add_done_callback(get_callback(publish_future, msg))
-    # publish_futures.append(publish_future)
-    # futures.wait(publish_futures, return_when=futures.ALL_COMPLETED)
-    # print(f"Published messages with error handler to {topic_path}.")
+    publish_future = publisher.publish(topic_path, msg.encode("utf-8"), **attributes)
+    publish_future.add_done_callback(get_callback(publish_future, msg))
+    publish_futures.append(publish_future)
+    futures.wait(publish_futures, return_when=futures.ALL_COMPLETED)
+    print(f"Published messages with error handler to {topic_path}.")

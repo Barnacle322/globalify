f5535ab2c5313086b83a16c496cb9ee92824ada3
Agahan14 <Imamidinov.agahan06@gmail.com>
2024-02-28 19:01:04 +0600

    💬 ✅ rename and add some tests for settings

--- src/tests/routes/test_settings.py
+++ src/tests/routes/test_settings.py
@@ -133,7 +133,7 @@ def test_settings_anonymous_get(client):
     assert b"Oops! Looks like you aren&#39;t logged in" in response.data
 
 
-def test_settings_authenticated_unverified_get(client, unverified_user, app, monkeypatch):
+def test_settings_unverified_get(client, unverified_user, app, monkeypatch):
     app.config["SERVER_NAME"] = "localhost"
     app.config["APPLICATION_ROOT"] = ""
     app.config["PREFERRED_URL_SCHEME"] = "http"
@@ -157,7 +157,7 @@ def test_settings_authenticated_unverified_get(client, unverified_user, app, mon
         assert b"Verify" in response.data
 
 
-def test_settings_authenticated_get(client, verified_user, app, monkeypatch):
+def test_settings_verified_get(client, verified_user, app, monkeypatch):
     app.config["SERVER_NAME"] = "localhost"
     app.config["APPLICATION_ROOT"] = ""
     app.config["PREFERRED_URL_SCHEME"] = "http"
@@ -177,7 +177,15 @@ def test_settings_authenticated_get(client, verified_user, app, monkeypatch):
         assert b"Use a permanent address where you can receive mail." in response.data
 
 
-def test_settings_security_authenticated_unverified_get(client, unverified_user, app, monkeypatch):
+def test_settings_security_anonymous_get(client):
+    response = client.get("/settings/security", follow_redirects=True)
+    assert response.status_code == 200
+    assert b"Welcome!" in response.data
+    assert b"Sign in with your social media" in response.data
+    assert b"Oops! Looks like you aren&#39;t logged in" in response.data
+
+
+def test_settings_security_unverified_get(client, unverified_user, app, monkeypatch):
     app.config["SERVER_NAME"] = "localhost"
     app.config["APPLICATION_ROOT"] = ""
     app.config["PREFERRED_URL_SCHEME"] = "http"
@@ -200,7 +208,7 @@ def test_settings_security_authenticated_unverified_get(client, unverified_user,
         assert b"Verify" in response.data
 
 
-def test_settings_security_authenticated_get(client, verified_user, app, monkeypatch):
+def test_settings_security_verified_get(client, verified_user, app, monkeypatch):
     app.config["SERVER_NAME"] = "localhost"
     app.config["APPLICATION_ROOT"] = ""
     app.config["PREFERRED_URL_SCHEME"] = "http"
@@ -224,15 +232,15 @@ def test_settings_security_authenticated_get(client, verified_user, app, monkeyp
         assert b"Yes, delete my account" in response.data
 
 
-def test_settings_security_anonymous_get(client):
-    response = client.get("/settings/security", follow_redirects=True)
+def test_settings_plan_anonymous_get(client):
+    response = client.get("/settings/plan", follow_redirects=True)
     assert response.status_code == 200
     assert b"Welcome!" in response.data
     assert b"Sign in with your social media" in response.data
     assert b"Oops! Looks like you aren&#39;t logged in" in response.data
 
 
-def test_settings_plan_authenticated_unverified_get(client, unverified_user, app, monkeypatch):
+def test_settings_plan_unverified_get(client, unverified_user, app, monkeypatch):
     app.config["SERVER_NAME"] = "localhost"
     app.config["APPLICATION_ROOT"] = ""
     app.config["PREFERRED_URL_SCHEME"] = "http"
@@ -256,7 +264,7 @@ def test_settings_plan_authenticated_unverified_get(client, unverified_user, app
         assert b"Verify" in response.data
 
 
-def test_settings_plan_authenticated_get(client, verified_user, app, monkeypatch):
+def test_settings_plan_verified_get(client, verified_user, app, monkeypatch):
     app.config["SERVER_NAME"] = "localhost"
     app.config["APPLICATION_ROOT"] = ""
     app.config["PREFERRED_URL_SCHEME"] = "http"
@@ -276,7 +284,7 @@ def test_settings_plan_authenticated_get(client, verified_user, app, monkeypatch
         assert b"This is your current subscription. You can update your plan any time." in response.data
 
 
-def test_settings_plan_anonymous_get(client):
+def test_settings_billing_anonymous_get(client):
     response = client.get("/settings/plan", follow_redirects=True)
     assert response.status_code == 200
     assert b"Welcome!" in response.data
@@ -284,7 +292,7 @@ def test_settings_plan_anonymous_get(client):
     assert b"Oops! Looks like you aren&#39;t logged in" in response.data
 
 
-def test_settings_billing_authenticated_unverified_get(client, unverified_user, app, monkeypatch):
+def test_settings_billing_unverified_get(client, unverified_user, app, monkeypatch):
     with patch("stripe.Invoice.list") as mock_invoice_list:
         mock_invoice_list.return_value = [
             {
@@ -321,7 +329,7 @@ def test_settings_billing_authenticated_unverified_get(client, unverified_user, 
             assert b"Verify" in response.data
 
 
-def test_settings_billing_authenticated_get(client, verified_user, app, monkeypatch):
+def test_settings_billing_verified_get(client, verified_user, app, monkeypatch):
     with patch("stripe.Invoice.list") as mock_invoice_list:
         mock_invoice_list.return_value = [
             {
@@ -355,15 +363,63 @@ def test_settings_billing_authenticated_get(client, verified_user, app, monkeypa
             assert b"See your invoice history here." in response.data
 
 
-def test_settings_billing_anonymous_get(client):
-    response = client.get("/settings/plan", follow_redirects=True)
-    assert response.status_code == 200
-    assert b"Welcome!" in response.data
-    assert b"Sign in with your social media" in response.data
-    assert b"Oops! Looks like you aren&#39;t logged in" in response.data
+def test_anonymous_user_change_personal_info(client, app, monkeypatch):
+    app.config["SERVER_NAME"] = "localhost"
+    app.config["APPLICATION_ROOT"] = ""
+    app.config["PREFERRED_URL_SCHEME"] = "http"
+    with app.app_context():
+        response = client.post(
+            "/settings/personal-info",
+            data={
+                "first-name": "NewFirstName",
+                "last-name": "NewLastName",
+                "username": "newusername",
+                "bio": "New bio",
+            },
+            follow_redirects=True,
+        )
+
+        assert response.status_code == 200
+        assert b"Welcome!" in response.data
+        assert b"Sign in with your social media" in response.data
+        assert b"Oops! Looks like you aren&#39;t logged in" in response.data
 
 
-def test_settings_change_personal_info(client, verified_user, app, monkeypatch):
+def test_unverified_user_change_personal_info(client, unverified_user, app, monkeypatch):
+    app.config["SERVER_NAME"] = "localhost"
+    app.config["APPLICATION_ROOT"] = ""
+    app.config["PREFERRED_URL_SCHEME"] = "http"
+    with app.app_context():
+        mock_authorize = MagicMock(
+            return_value={"userinfo": {"email": "johndoe@example.com", "given_name": "Test", "family_name": "User"}}
+        )
+        monkeypatch.setattr(oauth.google, "authorize_access_token", mock_authorize)
+
+        response = client.get(url_for("auth.google_callback"), follow_redirects=True)
+
+        client.post("/login", data=dict(email="johndoe@example.com"), follow_redirects=True)
+
+        response = client.post(
+            "/settings/personal-info",
+            data={
+                "first-name": "NewFirstName",
+                "last-name": "NewLastName",
+                "username": "newusername",
+                "bio": "New bio",
+            },
+            follow_redirects=True,
+        )
+
+        assert response.status_code == 200
+        assert b"Email Verification" in response.data
+        assert (
+            b"A verification email has been sent to you! Click the link or input a code to verify your email address."
+            in response.data
+        )
+        assert b"Verify" in response.data
+
+
+def test_verified_user_change_personal_info(client, verified_user, app, monkeypatch):
     app.config["SERVER_NAME"] = "localhost"
     app.config["APPLICATION_ROOT"] = ""
     app.config["PREFERRED_URL_SCHEME"] = "http"
@@ -391,16 +447,16 @@ def test_settings_change_personal_info(client, verified_user, app, monkeypatch):
         assert response.status_code == 200
         assert b"Personal info successfully changed." in response.data
 
-        #ToDo Fix this
+        #TODO Fix this
         updated_user = User.get_by_id(1)
         assert updated_user is not None
-        assert updated_user.user_info.first_name == "NewFirstName" # type: ignore
-        assert updated_user.user_info.last_name == "NewLastName" # type: ignore
-        assert updated_user.user_info.username == "newusername" # type: ignore
-        assert updated_user.user_info.bio == "New bio" # type: ignore
+        assert updated_user.user_info.first_name == "NewFirstName"  # type: ignore
+        assert updated_user.user_info.last_name == "NewLastName"  # type: ignore
+        assert updated_user.user_info.username == "newusername"  # type: ignore
+        assert updated_user.user_info.bio == "New bio"  # type: ignore
 
 
-def test_change_personal_info_empty_first_name(client, verified_user, app, monkeypatch):
+def test_verified_user_change_personal_info_empty_first_name(client, verified_user, app, monkeypatch):
     app.config["SERVER_NAME"] = "localhost"
     app.config["APPLICATION_ROOT"] = ""
     app.config["PREFERRED_URL_SCHEME"] = "http"
@@ -426,7 +482,7 @@ def test_change_personal_info_empty_first_name(client, verified_user, app, monke
         assert b"First name cannot be empty." in response.data
 
 
-def test_change_personal_info_empty_last_name(client, verified_user, app, monkeypatch):
+def test_verified_user_change_personal_info_empty_last_name(client, verified_user, app, monkeypatch):
     app.config["SERVER_NAME"] = "localhost"
     app.config["APPLICATION_ROOT"] = ""
     app.config["PREFERRED_URL_SCHEME"] = "http"
@@ -452,7 +508,7 @@ def test_change_personal_info_empty_last_name(client, verified_user, app, monkey
         assert b"Last name cannot be empty." in response.data
 
 
-def test_change_personal_info_empty_bio(client, verified_user, app, monkeypatch):
+def test_verified_user_change_personal_info_empty_bio(client, verified_user, app, monkeypatch):
     app.config["SERVER_NAME"] = "localhost"
     app.config["APPLICATION_ROOT"] = ""
     app.config["PREFERRED_URL_SCHEME"] = "http"
@@ -479,7 +535,7 @@ def test_change_personal_info_empty_bio(client, verified_user, app, monkeypatch)
         assert b"Bio cannot be empty." in response.data
 
 
-def test_change_personal_info_empty_username(client, verified_user, app, monkeypatch):
+def test_verified_user_change_personal_info_empty_username(client, verified_user, app, monkeypatch):
     app.config["SERVER_NAME"] = "localhost"
     app.config["APPLICATION_ROOT"] = ""
     app.config["PREFERRED_URL_SCHEME"] = "http"
@@ -505,7 +561,7 @@ def test_change_personal_info_empty_username(client, verified_user, app, monkeyp
         assert b"Username cannot be empty." in response.data
 
 
-def test_change_personal_info_taken_username(client, verified_user, username_taken_user, app, monkeypatch):
+def test_verified_user_change_personal_info_taken_username(client, verified_user, username_taken_user, app, monkeypatch):
     app.config["SERVER_NAME"] = "localhost"
     app.config["APPLICATION_ROOT"] = ""
     app.config["PREFERRED_URL_SCHEME"] = "http"
@@ -582,7 +638,31 @@ def test_settings_company_anonymous_get(client):
     assert b"Oops! Looks like you aren&#39;t logged in" in response.data
 
 
-def test_settings_company_authenticated_get(client, new_user_with_company, app, monkeypatch):
+def test_settings_company_unverified_get(client, unverified_user, app, monkeypatch):
+    app.config["SERVER_NAME"] = "localhost"
+    app.config["APPLICATION_ROOT"] = ""
+    app.config["PREFERRED_URL_SCHEME"] = "http"
+    with app.app_context():
+        mock_authorize = MagicMock(
+            return_value={"userinfo": {"email": "johndoe@example.com", "given_name": "Test", "family_name": "User"}}
+        )
+        monkeypatch.setattr(oauth.google, "authorize_access_token", mock_authorize)
+
+        response = client.get(url_for("auth.google_callback"), follow_redirects=True)
+
+        client.post("/login", data=dict(email="johndoe@example.com", password="password"), follow_redirects=True)
+
+        response = client.get("/settings/company", follow_redirects=True)
+        assert response.status_code == 200
+        assert b"Email Verification" in response.data
+        assert (
+            b"A verification email has been sent to you! Click the link or input a code to verify your email address."
+            in response.data
+        )
+        assert b"Verify" in response.data
+
+
+def test_settings_company_verified_get(client, new_user_with_company, app, monkeypatch):
     app.config["SERVER_NAME"] = "localhost"
     app.config["APPLICATION_ROOT"] = ""
     app.config["PREFERRED_URL_SCHEME"] = "http"
@@ -598,12 +678,11 @@ def test_settings_company_authenticated_get(client, new_user_with_company, app, 
 
         response = client.get("/settings/company", follow_redirects=True)
         assert response.status_code == 200
-        print(response.data)
         assert b"Company Information" in response.data
         assert b"Update your company information here." in response.data
 
 
-def test_settings_company_authenticated_without_company_get(client, verified_user, app, monkeypatch):
+def test_verified_user_without_company_get(client, verified_user, app, monkeypatch):
     app.config["SERVER_NAME"] = "localhost"
     app.config["APPLICATION_ROOT"] = ""
     app.config["PREFERRED_URL_SCHEME"] = "http"
@@ -623,7 +702,7 @@ def test_settings_company_authenticated_without_company_get(client, verified_use
         assert response.status_code == 404
 
 
-def test_change_company_empty_name(client, new_user_with_company, app, monkeypatch):
+def test_verified_user_change_company_empty_name(client, new_user_with_company, app, monkeypatch):
     app.config["SERVER_NAME"] = "localhost"
     app.config["APPLICATION_ROOT"] = ""
     app.config["PREFERRED_URL_SCHEME"] = "http"
@@ -649,7 +728,7 @@ def test_change_company_empty_name(client, new_user_with_company, app, monkeypat
         assert b"Company name cannot be empty." in response.data
 
 
-def test_change_company_empty_industry_and_round(client, new_user_with_company, app, monkeypatch):
+def test_verified_user_change_company_empty_industry_and_round(client, new_user_with_company, app, monkeypatch):
     app.config["SERVER_NAME"] = "localhost"
     app.config["APPLICATION_ROOT"] = ""
     app.config["PREFERRED_URL_SCHEME"] = "http"
@@ -676,7 +755,7 @@ def test_change_company_empty_industry_and_round(client, new_user_with_company, 
         assert b"Please select rounds and industries." in response.data
 
 
-def test_change_company_empty_country(client, new_user_with_company, app, monkeypatch):
+def test_verified_user_change_company_empty_country(client, new_user_with_company, app, monkeypatch):
     app.config["SERVER_NAME"] = "localhost"
     app.config["APPLICATION_ROOT"] = ""
     app.config["PREFERRED_URL_SCHEME"] = "http"
@@ -703,7 +782,7 @@ def test_change_company_empty_country(client, new_user_with_company, app, monkey
         assert b"Country ID is required." in response.data
 
 
-def test_change_company_valid_data(client, new_user_with_company, app, monkeypatch):
+def test_verified_user_change_company_valid_data(client, new_user_with_company, app, monkeypatch):
     app.config["SERVER_NAME"] = "localhost"
     app.config["APPLICATION_ROOT"] = ""
     app.config["PREFERRED_URL_SCHEME"] = "http"

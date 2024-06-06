14d8c4e5dff3ec1efe03f96096fee490e4fd4cca
Agahan14 <Imamidinov.agahan06@gmail.com>
2024-06-06 18:34:46 +0600

    🐛 ♻️ fix bug with search

--- src/project/static/scripts/admin/investmentFirms.js
+++ src/project/static/scripts/admin/investmentFirms.js
@@ -16,38 +16,3 @@ async function deleteInvestmentFirm(id) {
         window.location.reload();
     }
 }
-
-document.getElementById("search-btn").addEventListener("click", function (event) {
-    search();
-});
-
-function search() {
-    const searchQuery = document.getElementById("search").value;
-
-    const paramsArray = getExistingParams(["search", "page"]);
-
-    if (searchQuery !== "") paramsArray.unshift(`search=${encodeURIComponent(searchQuery)}`);
-
-    addParamsToUrl(paramsArray);
-}
-
-function getExistingParams(excludedParams) {
-    const urlParams = new URLSearchParams(window.location.search);
-    let paramsArray = [];
-
-    for (let param of urlParams) {
-        if (!excludedParams.includes(param[0])) {
-            paramsArray.push(`${param[0]}=${encodeURIComponent(param[1])}`);
-        }
-    }
-
-    return paramsArray;
-}
-
-function addParamsToUrl(paramsArray) {
-    const paramsString = paramsArray.length > 0 ? "?" + paramsArray.join("&") : "";
-    const baseUrl = window.location.href.split("?")[0];
-    const newUrl = baseUrl + paramsString;
-
-    window.location.href = newUrl;
-}

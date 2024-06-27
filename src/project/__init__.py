8b171277e6b58ca9cead490484b972ab01548025
Barnacle322 <arstan.usenov@gmail.com>
2024-06-28 02:01:31 +0600

    🎨 Formatting

--- src/project/__init__.py
+++ src/project/__init__.py
@@ -29,7 +29,6 @@ def create_app(database_url="sqlite:///db.sqlite"):
     app.config["SQLALCHEMY_POOL_RECYCLE"] = int(os.getenv("SQLALCHEMY_POOL_RECYCLE", 1800))
     app.config["SQLALCHEMY_ENGINE_OPTIONS"] = {"pool_pre_ping": True}
     app.config["REMEMBER_COOKIE_DURATION"] = timedelta(days=30)
-    app.config["SQLALCHEMY_ECHO"] = True
 
     app.secret_key = os.getenv("SECRET_KEY", "18c2ff95-83a1-4998-8bee-0c6a2170497c")
 
@@ -42,6 +41,7 @@ def create_app(database_url="sqlite:///db.sqlite"):
         app.config["DEBUG_TB_INTERCEPT_REDIRECTS"] = False
         app.config["SQLALCHEMY_RECORD_QUERIES"] = True
         app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = True
+        app.config["SQLALCHEMY_ECHO"] = True
         # app.config["DEBUG_TB_PROFILER_ENABLED"] = True
         # toolbar.init_app(app)
     else:

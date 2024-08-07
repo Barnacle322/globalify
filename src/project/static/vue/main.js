01f96eed3715828f63cb42eeeda9232717bea5ab
Agahan14 <Imamidinov.agahan06@gmail.com>
2024-08-07 11:59:57 +0600

    🔥 ♻️ Refactor bookmark button event handlers to use Vue's @click directive and delete unnecessary js files

--- src/project/static/vue/main.js
+++ src/project/static/vue/main.js
@@ -276,5 +276,51 @@ createApp({
                 link.setAttribute("href", this.applyQueryParams(link.getAttribute("href")));
             });
         },
+        async toggleInvestorBookmark(investorId) {
+            const csrfToken = document.getElementById("csrf_token").value;
+            try {
+                const response = await fetch(`/investor/${investorId}/bookmark`, {
+                    method: "POST",
+                    headers: {
+                        "Content-Type": "application/json",
+                        "X-CSRFToken": csrfToken,
+                    },
+                });
+                if (response.ok) {
+                    const data = await response.json();
+                    var svg = document.getElementById(`bookmark-svg-investor-${investorId}`);
+                    if (data[0].bookmarked) {
+                        svg.style.fill = "#FFC9FC";
+                    } else {
+                        svg.style.fill = "none";
+                    }
+                }
+            } catch (error) {
+                console.error(error);
+            }
+        },
+        async toggleInvestmentFirmBookmark(firmId) {
+            const csrfToken = document.getElementById("csrf_token").value;
+            try {
+                const response = await fetch(`/investment-firm/${firmId}/bookmark`, {
+                    method: "POST",
+                    headers: {
+                        "Content-Type": "application/json",
+                        "X-CSRFToken": csrfToken,
+                    },
+                });
+                if (response.ok) {
+                    const data = await response.json();
+                    var svg = document.getElementById(`bookmark-svg-firm-${firmId}`);
+                    if (data[0].bookmarked) {
+                        svg.style.fill = "#FFC9FC";
+                    } else {
+                        svg.style.fill = "none";
+                    }
+                }
+            } catch (error) {
+                console.error(error);
+            }
+        },
     },
 }).mount("#app");

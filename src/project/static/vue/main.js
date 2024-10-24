f5b1b456f5294d4f1c7a53114af04913ad454414
Agahan14 <Imamidinov.agahan06@gmail.com>
2024-10-25 02:52:01 +0600

    🐛 fix user profile - investor link

--- src/project/static/vue/main.js
+++ src/project/static/vue/main.js
@@ -80,12 +80,9 @@ createApp({
         async handleInvestorBookmark(data) {
             try {
                 if (data.status) {
-                    console.log(typeof data.investorId);
                     this.investorBookmakrIds.push(data.investorId); // Corrected typo
-                    console.log(this.investorBookmakrIds);
                 } else {
                     this.investorBookmakrIds = this.investorBookmakrIds.filter((id) => id !== data.investorId); // Corrected typo
-                    console.log(this.investorBookmakrIds);
                 }
             } catch (error) {
                 console.error("Error handling investor bookmark:", error);

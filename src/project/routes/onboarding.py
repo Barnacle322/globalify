14c6041f03a3d84599fa100da19ee2ede378a2ca
Barnacle322 <arstan.usenov@gmail.com>
2024-10-26 03:06:35 +0400

    🎨 Format

--- src/project/routes/onboarding.py
+++ src/project/routes/onboarding.py
@@ -35,8 +35,10 @@ onboarding = Blueprint("onboarding", __name__)
 @onboarding.route("/", methods=["GET"])
 @login_required
 def index():
-    claim_request = ClaimRequest.get_with_investor_by_user_id(current_user.id)
+    if current_user.is_authenticated and current_user.user_info.is_complete:
+        return redirect(url_for("main.search"))
 
+    claim_request = ClaimRequest.get_with_investor_by_user_id(current_user.id)
     return render_template("onboarding/index.html", claim_request=claim_request)
 
 

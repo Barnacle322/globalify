cd1d2826ec3ca076b12931bb4b384d856c6a5f84
Artur <artur@globalify.xyz>
2024-11-05 20:35:37 +0600

    Update main.js

--- src/project/static/vue/main.js
+++ src/project/static/vue/main.js
@@ -82,7 +82,6 @@ createApp({
             try {
                 if (data.status) {
                     this.investorBookmakrIds.push(data.investorId); // Corrected typo
-                    console.log(this.investorBookmakrIds);
                 } else {
                     this.investorBookmakrIds = this.investorBookmakrIds.filter((id) => id !== data.investorId); // Corrected typo
                 }
@@ -402,7 +401,7 @@ createApp({
             return url;
         },
         updateLinksWithQueryParams() {
-            document.querySelectorAll('a[href^="/"]:not([href^="//"])').forEach((link) => {
+            document.querySelectorAll("a[href^=\"/\"]:not([href^=\"//\"])").forEach((link) => {
                 if (!link.getAttribute("href").includes("search")) return;
                 link.setAttribute("href", this.applyQueryParams(link.getAttribute("href")));
             });

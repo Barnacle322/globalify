05f26adc6ec75ec7af54a87dd468ecf9f4678835
Artur <artur@glbalify.xyz>
2025-03-07 16:37:14 +0600

    ✨ uploading PDF

--- src/project/routes/presentation.py
+++ src/project/routes/presentation.py
@@ -1,11 +1,4 @@
-from datetime import datetime
-
-from flask import (
-    Blueprint,
-    jsonify,
-    render_template,
-    request,
-)
+from flask import Blueprint, redirect, render_template, request
 from flask_login import login_required
 
 from ..extensions import db
@@ -29,14 +22,44 @@ def index():
     )
 
 
-@presentation.route("/upload", methods=["GET"])
+presentation.route("/upload", methods=["GET", "POST"])
+
+
+@presentation.route("/upload", methods=["GET", "POST"])
 @login_required
 def upload_presentation():
+    # Обработка GET-запроса (отображение формы)
     status_type, msg = None, None
     if query := request.args:
         status_type = query.get("type")
         msg = query.get("msg")
-   
+
+    if "file" not in request.files:
+        print("No file part")
+        return render_template(
+            "presentation.html",
+            status_type=status_type,
+            msg=msg,
+        )
+
+    file = request.files["file"]
+
+    if file.filename == "":
+        print("No selected file")
+        return render_template(
+            "presentation.html",
+            status_type=status_type,
+            msg=msg,
+        )
+
+    try:
+        pdf_data = file.read()
+        print("File loaded successfully")
+        print(f"Size: {len(pdf_data)} bytes")
+
+    except Exception as e:
+        print(f"Error: {e}")
+
     return render_template(
         "presentation.html",
         status_type=status_type,
---
+++ src/project/static/vue/presentation.js
@@ -0,0 +1,65 @@
+createApp({
+    components: {
+        AsideComponent,
+        AsideMobileComponent,
+        NavbarComponent,
+    },
+    watch: {
+        asideMinified(value) {
+            localStorage.setItem("asideMinified", value);
+        },
+    },
+    mounted() {
+        document.addEventListener("click", this.handleClickOutside);
+        this.asideMinified = localStorage.getItem("asideMinified") == "true";
+    },
+    methods: {
+        handleFileUpload(event) {
+            this.file = event.target.files[0];
+            this.filename = this.file ? this.file.name : null;
+            console.log("File:", this.file);
+        },
+        async uploadFile() {
+            if (!this.file) {
+                this.uploadStatus = "Loading.";
+                return;
+            }
+
+            const formData = new FormData();
+            formData.append("file", this.file);
+
+            try {
+                const response = await fetch("/presentation/upload", {
+                    method: "POST",
+                    headers: {
+                        "X-CSRFToken": document.getElementById("csrf_token").value,
+                    },
+                    body: formData,
+                });
+
+                if (response.ok) {
+                    this.uploadStatus = "Success!";
+                    this.filename = null;
+                    this.file = null;
+                } else {
+                    const data = await response.json();
+                    this.uploadStatus = `Error: ${data.message || response.statusText}`;
+                }
+            } catch (error) {
+                console.error("Error:", error);
+                this.uploadStatus = error;
+            }
+        },
+    },
+    data() {
+        return {
+            asideExpanded: false,
+            asideMinified: false,
+            openAdvanced: false,
+
+            filename: null,
+            file: null,
+            uploadStatus: null,
+        };
+    },
+}).mount("#app");

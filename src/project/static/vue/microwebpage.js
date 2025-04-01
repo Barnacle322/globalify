7c64e9e7b1ac4b700a5ab79cdad5aefefca16943
nooruzbaiibraimov <nooruzbay@gmail.com>
2025-04-01 17:08:51 +0600

    :memo: Vue is not working. Trying to fix

--- src/project/static/vue/microwebpage.js
+++ src/project/static/vue/microwebpage.js
@@ -1,28 +1,39 @@
 createApp({
+    components: {
+        AsideComponent,
+        AsideMobileComponent,
+        NavbarComponent,
+    },
+
+    watch: {
+        asideMinified(value) {
+            localStorage.setItem("asideMinified", value);
+        },
+    },
+    mounted() {
+        document.addEventListener("click", this.handleClickOutside);
+        this.asideMinified = localStorage.getItem("asideMinified") == "true";
+    },
 
 
     methods: {
-        submitForm() {
-            console.log('Form submitted:', this.form)
-        }
     },
 
 
     data(){
         return {
-                asideExpanded: false,
-                asideMinified: false,
-                form: {
-                    name: '',
-                    description: '',
-                    logo_url: '',
-                    website_url: '',
-                    contact_email: '',
-                    employee_number: 0
-                }
+            asideExpanded: false,
+            asideMinified: false,
+            openAdvanced: false,
+
+            form: {
+                name: '',
+                description: '',
+                logo_url: '',
+                website_url: '',
+                contact_email: '',
+                employee_number: 0
             }
         }
-
-
-
+    },
 }).mount("app")
